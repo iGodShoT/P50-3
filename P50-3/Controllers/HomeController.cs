a369fa4d95b78e4e2ae798a6c5f88cba68806353
iGodShoT <comar1234567890@gmail.com>
2024-09-21 10:53:16 +0300

    Добавьте файлы проекта.

---
+++ P50-3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P50_3.Models;
+using System.Diagnostics;
+
+namespace P50_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Catalog() 
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Андрей";
+            ViewData["Age"] = 12;
+            ViewData["Array"] = new List<string>();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        private string Some()
+        {
+            return "Hello";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P50-3/Controllers/SomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P50_3.Controllers
+{
+    public class SomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
